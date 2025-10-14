b36eea2da6c8a42c1f633d26c5990e1aee4c94ca
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-14 16:28:51 +0100

    create the specialite Service

--- src/main/java/org/medical/teleexpertisemedical/service/SpecialiteService.java
+++ src/main/java/org/medical/teleexpertisemedical/service/SpecialiteService.java
@@ -1,53 +1,30 @@
 package org.medical.teleexpertisemedical.service;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import org.medical.teleexpertisemedical.dao.SpecialiteDAO;
 import org.medical.teleexpertisemedical.entity.Specialite;
 
 import java.util.List;
 
 public class SpecialiteService {
-    private final EntityManagerFactory emf;
+    private SpecialiteDAO specialiteDAO;
 
     public SpecialiteService() {
-        this.emf = Persistence.createEntityManagerFactory("teleExpertisePU");
+           this.specialiteDAO = new SpecialiteDAO();
     }
 
     public Specialite findById(Long id) {
-        try (EntityManager em = emf.createEntityManager()) {
-            return em.find(Specialite.class, id);
-        }
+        return specialiteDAO.findById(id);
     }
 
     public List<Specialite> findAll() {
-        try (EntityManager em = emf.createEntityManager()) {
-            return em.createQuery("SELECT s FROM Specialite s ORDER BY s.nom", Specialite.class)
-                    .getResultList();
-        }
+        return specialiteDAO.findAll();
     }
 
     public void save(Specialite specialite) {
-        EntityManager em = emf.createEntityManager();
-        try {
-            em.getTransaction().begin();
-            if (specialite.getId() == null) {
-                em.persist(specialite);
-            } else {
-                em.merge(specialite);
-            }
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            em.getTransaction().rollback();
-            throw e;
-        } finally {
-            em.close();
-        }
+        specialiteDAO.save(specialite);
     }
 
     public void close() {
-        if (emf != null && emf.isOpen()) {
-            emf.close();
-        }
+       specialiteDAO.close();
     }
 }
