85e21fd2d993bbbaa3e1d2c33cd508e7f9f1f0d0
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-14 15:54:15 +0100

    Add the DemandeExpertise Service

--- src/main/java/org/medical/teleexpertisemedical/service/DemandeExpertiseService.java
+++ src/main/java/org/medical/teleexpertisemedical/service/DemandeExpertiseService.java
@@ -1,90 +1,45 @@
 package org.medical.teleexpertisemedical.service;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import org.medical.teleexpertisemedical.dao.DemandeExpertiseDAO;
 import org.medical.teleexpertisemedical.entity.DemandeExpertise;
 
 import java.util.List;
 
 public class DemandeExpertiseService {
-    private EntityManagerFactory emf;
+    private DemandeExpertiseDAO dao;
 
     public DemandeExpertiseService() {
-        this.emf = Persistence.createEntityManagerFactory("medical-pu");
+        this.dao = new DemandeExpertiseDAO();
     }
 
     public DemandeExpertise findById(Long id) {
-        EntityManager em = emf.createEntityManager();
-        try {
-            return em.find(DemandeExpertise.class, id);
-        } finally {
-            em.close();
-        }
+        return dao.findById(id);
     }
 
     public List<DemandeExpertise> findBySpecialisteId(Long specialisteId) {
-        EntityManager em = emf.createEntityManager();
-        try {
-            return em.createQuery(
-                            "SELECT d FROM DemandeExpertise d WHERE d.specialiste.id = :specialisteId ORDER BY d.dateDemande DESC",
-                            DemandeExpertise.class)
-                    .setParameter("specialisteId", specialisteId)
-                    .getResultList();
-        } finally {
-            em.close();
-        }
+        return dao.findBySpecialisteId(specialisteId);
     }
 
-    public List<DemandeExpertise> findBySpecialisteIdAndStatut(Long specialisteId, String statut) {
-        EntityManager em = emf.createEntityManager();
-        try {
-            return em.createQuery(
-                            "SELECT d FROM DemandeExpertise d WHERE d.specialiste.id = :specialisteId AND d.statut = :statut ORDER BY d.dateDemande DESC",
-                            DemandeExpertise.class)
-                    .setParameter("specialisteId", specialisteId)
-                    .setParameter("statut", statut)
-                    .getResultList();
-        } finally {
-            em.close();
-        }
+
+    public List<DemandeExpertise> findByConsultationId(Long consultationId) {
+        return dao.findByConsultationId(consultationId);
+    }
+
+    public List<DemandeExpertise> findAll() {
+        return dao.findAll();
     }
 
     public void save(DemandeExpertise demande) {
-        EntityManager em = emf.createEntityManager();
-        try {
-            em.getTransaction().begin();
-            if (demande.getId() == null) {
-                em.persist(demande);
-            } else {
-                em.merge(demande);
-            }
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            em.getTransaction().rollback();
-            throw e;
-        } finally {
-            em.close();
-        }
+        dao.save(demande);
     }
 
     public void update(DemandeExpertise demande) {
-        EntityManager em = emf.createEntityManager();
-        try {
-            em.getTransaction().begin();
-            em.merge(demande);
-            em.getTransaction().commit();
-        } catch (Exception e) {
-            em.getTransaction().rollback();
-            throw e;
-        } finally {
-            em.close();
-        }
+        dao.update(demande);
     }
 
     public void close() {
-        if (emf != null && emf.isOpen()) {
-            emf.close();
+        if (dao != null) {
+            dao.close();
         }
     }
 }
