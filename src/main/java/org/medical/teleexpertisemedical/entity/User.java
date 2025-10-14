c2f58e10e12530556b882b0735dab5c032accd39
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-14 14:39:33 +0100

    Edit in the User Entity pout ajouter un Specialiste

--- src/main/java/org/medical/teleexpertisemedical/entity/User.java
+++ src/main/java/org/medical/teleexpertisemedical/entity/User.java
@@ -33,7 +33,6 @@ public class User {
     private String telephone;
     private String email;
 
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
