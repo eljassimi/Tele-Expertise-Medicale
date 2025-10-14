8ba0dbafe3b77a8ce02be04bebbbbe05711e5902
eljassimi <abdelmouhaimineeljassimi@gmail.com>
2025-10-14 15:49:06 +0100

    add new function in the user service

--- src/main/java/org/medical/teleexpertisemedical/service/UserService.java
+++ src/main/java/org/medical/teleexpertisemedical/service/UserService.java
@@ -1,34 +1,44 @@
 package org.medical.teleexpertisemedical.service;
 
-import jakarta.ejb.EJB;
-import jakarta.ejb.Stateless;
 import org.medical.teleexpertisemedical.dao.UserDAO;
 import org.medical.teleexpertisemedical.entity.User;
 
 import java.util.List;
 
-@Stateless
 public class UserService {
-    @EJB
     private UserDAO userDAO;
 
     public UserService() {
         this.userDAO = new UserDAO();
     }
 
-    public void register(User user) {
-        userDAO.save(user);
+    public User save(User user) {
+        return userDAO.save(user);
     }
 
     public User findById(Long id) {
         return userDAO.findById(id);
     }
 
+    public User findByUsername(String username) {
+        return userDAO.findByUsername(username);
+    }
+
+    public boolean usernameExists(String username) {
+        return userDAO.usernameExists(username);
+    }
+
     public List<User> findByRole(String role) {
         return userDAO.findByRole(role);
     }
 
+    public List<User> findAll() {
+        return userDAO.findAll();
+    }
+
     public void close() {
-        userDAO.close();
+        if (userDAO != null) {
+            userDAO.close();
+        }
     }
 }
